d9df9068e8310fa6223940426213322af12722e2
Asamoah Kwaku Acheampong <asamoahak@live.co.uk>
2018-05-16 09:59:36 +0000

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pharmcode
                 </div>
 
                 <div class="links">

9409988611fe545c903ba704553d4d180adb1dca
Asamoah Kwaku Acheampong <asamoahak@live.co.uk>
2018-05-23 10:37:02 +0000

    ...

--- app/Http/Controllers/PrescriptionDetailController.php
+++ app/Http/Controllers/PrescriptionDetailController.php
@@ -43,14 +43,15 @@ class PrescriptionDetailController extends Controller
         //
         $random = $request->drug_id.$request->precription_id."pharmcode_qr".rand(1,100000).date('m');
 
-        $qr=QRCode::text($random)->png();
-        Storage::disk('local')->put('images/qrcodes'.'/'.$random, $qr, 'public');
-
-
-        $request->request->add(['qr_code_url' => $random]);
-        PrescriptionDetail::create($request->all());
-        $message="Prescription details created successfully";
-        return response(compact('message'),200);
+        return response(compact('random'),200);
+//        $qr=QRCode::text($random)->png();
+//        Storage::disk('local')->put('images/qrcodes'.'/'.$random, $qr, 'public');
+//
+//
+//        $request->request->add(['qr_code_url' => $random]);
+//        PrescriptionDetail::create($request->all());
+//        $message="Prescription details created successfully";
+//        return response(compact('message'),200);
     }
 
     /**
